6f767136fb87d8f63be824ff85d27adbb7ebaa28
brad oyler <bradoyler@gmail.com>
2017-07-18 10:21:43 -0400

    minor update to example

--- example/index.js
+++ example/index.js
@@ -1,9 +1,9 @@
 const output = require('d3node-output')
 const worldMap = require('../')
 const fs = require('fs')
-const csvString = fs.readFileSync('data/world-pop.tsv').toString()
+const data = fs.readFileSync('data/world-pop.tsv').toString()
 const d3 = require('d3')
-const populationsByCountry = d3.tsvParse(csvString)
+const populationsByCountry = d3.tsvParse(data)
 
 // create output files
 const options = {} // use defaults
